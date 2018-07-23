ec40df4260f462d6b0774c44c8ba52c57344c4df
Windrushfarer <windrushfarer@gmail.com>
2018-07-23 21:44:48 +0500

    Initial project

---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// require('./Footer.css');
+
+export default class Footer extends React.PureComponent {
+  render() {
+    return (
+      <div className='Footer'>
+        <p>&copy; 2018 Блог крутого разработчика</p>
+        <p>Другие способы связи:</p>
+        <p><a href="https://www.facebook.com/">Facebook</a></p>
+        <p><a href="https://www.twitter.com/">Twitter</a></p>
+      </div>
+    )
+  }
+}
